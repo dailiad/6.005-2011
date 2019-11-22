00baa4250dab6ab303f3cdabf62a181542bb2cd0
戴李 <863122294@qq.com>
2019-11-22 16:30:36 +0800

    finish problem04: WordFinder and WordFinderTest

--- ps1/src/piwords/WordFinder.java
+++ ps1/src/piwords/WordFinder.java
@@ -5,21 +5,46 @@ import java.util.Map;
 
 public class WordFinder {
     /**
-     * Given a String (the haystack) and an array of Strings (the needles),
-     * return a Map<String, Integer>, where keys in the map correspond to
-     * elements of needles that were found as substrings of haystack, and the
-     * value for each key is the lowest index of haystack at which that needle
-     * was found. A needle that was not found in the haystack should not be
-     * returned in the output map.
+     * Given a String (the haystack) and an array of Strings (the needles), return a
+     * Map<String, Integer>, where keys in the map correspond to elements of needles
+     * that were found as substrings of haystack, and the value for each key is the
+     * lowest index of haystack at which that needle was found. A needle that was
+     * not found in the haystack should not be returned in the output map.
      * 
      * @param haystack The string to search into.
-     * @param needles The array of strings to search for. This array is not
-     *                mutated.
+     * @param needles  The array of strings to search for. This array is not
+     *                 mutated.
      * @return The list of needles that were found in the haystack.
      */
-    public static Map<String, Integer> getSubstrings(String haystack,
-                                                     String[] needles) { 
-        // TODO: Implement (Problem 4.b)
-        return new HashMap<String, Integer>();
+    public static Map<String, Integer> getSubstrings(String haystack, String[] needles) {
+        Map<String, Integer> subStrings = new HashMap<>();
+        char firstChar = 0;
+        int matchIndex = 0;
+        int fromIndex = 0;
+        for (String needle : needles) {
+            fromIndex = 0;
+            matchIndex = 0;
+            if (needle.length() == 0) {
+                continue;
+            }
+            firstChar = needle.charAt(0);
+            while (matchIndex < haystack.length()) {
+                matchIndex = haystack.indexOf(firstChar, fromIndex);
+                if (matchIndex != -1 && matchIndex + needle.length() <= haystack.length()) {
+                    String subString = haystack.substring(matchIndex, matchIndex + needle.length());
+                    if (subString.equals(needle)) {
+                        subStrings.put(needle, matchIndex);
+                        break;
+                    } else {
+                        fromIndex = matchIndex + 1;
+                    }
+                } else {
+                    break;
+                }
+            }
+
+        }
+
+        return subStrings;
     }
 }
--- ps1/src/piwords/WordFinderTest.java
+++ ps1/src/piwords/WordFinderTest.java
@@ -22,5 +22,75 @@ public class WordFinderTest {
                                                               needles));
     }
 
-    // TODO: Write more tests (Problem 4.a)
+    // test strategy for Map<String, Integer> getSubstrings(String haystack,
+    //                                                      String[] needles):
+    // haystack: haystack.length() = 0, haystack.length() > 0
+    // needles: needles.length = 0, needles.length > 0
+    // needles elements's length: 1, >1
+    // times needles[i] appears in haystack: 0, 1, n
+    // locations needles[i] appears in haystack: first, middle, end
+    
+    // test haystack.length() = 0
+    @Test
+    public void testEmptyHaystack() {
+        String haystack = "";
+        String[] needles = {"ab", "bc"};
+        
+        Map<String, Integer> expectedOutput = new HashMap<>();
+        
+        assertEquals(expectedOutput, WordFinder.getSubstrings(haystack, needles));
+    }
+    
+    // test needles.length = 0
+    @Test
+    public void testEmptyNeedles() {
+        String haystack = "abcdefg";
+        String[] needles = {};
+        
+        Map<String, Integer> expectedOutput = new HashMap<>();
+        
+        assertEquals(expectedOutput, WordFinder.getSubstrings(haystack, needles));
+    }
+    
+    // test times needles[i] appears in haystack: 0, 1, n
+    @Test
+    public void testTimesAppearsInHaystack() {
+        // "ab" 3 times, "bcd" 1 times, "ij" 0 times
+        String haystack = "abcdeabfghiabj";
+        String[] needles = {"ab", "bcd", "ij"};
+        
+        Map<String, Integer> expectedOutput = new HashMap<>();
+        expectedOutput.put("ab", 0);
+        expectedOutput.put("bcd", 1);
+        
+        assertEquals(expectedOutput, WordFinder.getSubstrings(haystack, needles));
+    }
+    
+    // test needles elements's length appears in needles: 1, >1
+    @Test
+    public void testLengthOfNeedlesElementAppearINNeedles() {
+        // length "": 0, "a": 1, "ab": 2, "def": 3
+        String haystack = "abcdefg";
+        String[] needles = {"", "a", "bc", "def"};
+        
+        Map<String, Integer> expectedOutput = new HashMap<>();
+        expectedOutput.put("a", 0);
+        expectedOutput.put("bc", 1);
+        expectedOutput.put("def", 3);
+    }
+    
+    // test locations needles[i] appears in haystack: first, middle, end
+    @Test
+    public void testLocationsElementAppearsInHaystack() {
+        // "abc":first, "de":middle, "g":end
+        String haystack = "abcdefg";
+        String[] needles = {"abc", "de", "g"};
+        
+        Map<String, Integer> expectedOutput = new HashMap<>();
+        expectedOutput.put("abc", 0);
+        expectedOutput.put("de", 3);
+        expectedOutput.put("g", 6);
+        
+        assertEquals(expectedOutput, WordFinder.getSubstrings(haystack, needles));
+    }
 }
